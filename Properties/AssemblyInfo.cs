1689b863ba4e797ef81d0646835c78b33c9d5583
Mikael Santrolli <mikael.santrolli@gmail.com>
2020-02-18 17:55:40 +0000

    Updated Installed Files

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AutoSync")]
 [assembly: AssemblyDescription( "AutoSync" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Foster + Partners")]
-[assembly: AssemblyProduct("AutoSync Revit C# .NET Add-In")]
-[assembly: AssemblyCopyright("Copyright 2019 (C) Foster + Partners")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoSync")]
+[assembly: AssemblyCopyright("Copyright © 2020 Mikael Santrolli")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.0.0.0")]
-[assembly: AssemblyFileVersion("2019.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
